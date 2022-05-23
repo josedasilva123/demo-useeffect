c183ac5b16e6ee677bfee9569cb28fcd2c35f1bd
Alex Conder <alex@kenzie.com.br>
2022-05-23 18:31:34 -0300

    feat: localStorage counter width useEffect

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,8 +1,24 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { StyledCounter } from './styles';
 
 const Counter = () => {
   const [counter, setCounter] = useState(0);  
+
+  //DESMONTAGEM
+  // A função declarada no primeiro parâmetro + a lista de dependências vazia dispara um efeito na montagem
+  useEffect(() => {
+    // console.log('Ocorreu o efeito de montagem');
+    const counterStorage = +localStorage.getItem('EXAMPLE_COUNTER');
+    setCounter(counterStorage);
+  }, [])
+
+  //ATUALIZAÇÃO
+  //Cada vez que um item da lista de dependencias é alterado o efeito dispara
+  useEffect(() => {   
+    // console.log('O contador foi alterado')
+    localStorage.setItem("EXAMPLE_COUNTER", counter);
+  }, [counter])
+  
   return (
     <StyledCounter>
         <h1>{counter}</h1>
