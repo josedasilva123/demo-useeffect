cfa587defff3e25267e44d03d0c28df90d574b3a
Alex Conder <alex@kenzie.com.br>
2022-05-23 18:31:57 -0300

    feat: get pokemon list from API and render with useEffect

--- src/components/PokemonList/index.jsx
+++ src/components/PokemonList/index.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeContainer } from "../../styles/globals";
 import Header from "./Header";
 import PokemonCard from "./PokemonCard";
 import { StyledList } from "./styles";
 
 const PokemonList = () => {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    /*  
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+    .then(res => res.json())
+    .then(res => setPokemons(res.results));
+    */
+    async function getPokemons(){
+        try {
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+            const json = await response.json();  
+            setPokemons(json.results);
+        } catch (error) {
+            console.log('Deu zika!')   
+        }        
+    }
+    getPokemons();
+  }, []);
+
   return (
     <>
       <Header />
       <ThemeContainer>
         <StyledList>
-          <PokemonCard name="Bulbasaur" />
-          <PokemonCard name="Charmander" />
-          <PokemonCard name="Squirtle" />          
+           {pokemons?.length > 0 && pokemons.map((pokemon) => (
+               <PokemonCard name={pokemon.name} />
+           ))}
+          
         </StyledList>
       </ThemeContainer>
     </>
