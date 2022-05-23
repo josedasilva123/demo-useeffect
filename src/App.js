469359b3b82be6faa535f6ffd59695a6d9ce975b
Alex Conder <alex@kenzie.com.br>
2022-05-23 18:32:03 -0300

    feat: import components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import PokemonList from "./components/PokemonList";
 import { GlobalStyle } from "./styles/globals";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <PokemonList />
       <Counter />
     </div>
   );
